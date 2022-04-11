f5e2fbd34c392760a6ca1ed50482545e91cd408a
felgarti <elgartifatima@gmail.com>
2022-04-11 02:59:45 +0200

    part 2

--- src/main/java/com/example/patientsmvc/entities/Patient.java
+++ src/main/java/com/example/patientsmvc/entities/Patient.java
@@ -4,19 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Generated;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Date;
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id ;
+    @NotEmpty
+    @Size(min=4 , max=40)
     private String  nom ;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance ;
     private boolean malade ;
+    @DecimalMin("0")
     private int score ;
 }
