4444fcd473f4a950561a7d689ed657b481dab4e4
felgarti <elgartifatima@gmail.com>
2022-04-26 14:32:04 +0000

    recherche multi critères : nom et age

--- src/main/java/com/example/patientsmvc/repositories/PatientRepository.java
+++ src/main/java/com/example/patientsmvc/repositories/PatientRepository.java
@@ -4,7 +4,19 @@ import com.example.patientsmvc.entities.Patient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.hibernate.hql.internal.antlr.SqlTokenTypes.OR;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
-Page<Patient> findByNomContains(String n  , Pageable pageable) ;
+
+    Page<Patient> findByNomOrScore(String n , int s , Pageable pageable);
+Page<Patient> findByNomContains(String n , Pageable pageable) ;
+
+
+
 }
--- src/main/java/com/example/patientsmvc/web/PatientController.java
+++ src/main/java/com/example/patientsmvc/web/PatientController.java
@@ -2,6 +2,7 @@ package com.example.patientsmvc.web;
 
 import com.example.patientsmvc.entities.Patient;
 import com.example.patientsmvc.repositories.PatientRepository;
+import javassist.compiler.ast.Keyword;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 import org.springframework.data.domain.Page;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.naming.Binding;
 import javax.validation.Valid;
+import java.security.Key;
 import java.util.List;
 
 @Controller
@@ -23,12 +25,32 @@ import java.util.List;
 
 public class PatientController {
     private PatientRepository patientRepository ;
+    public boolean isInteger( String input ) {
+        try {
+            Integer.parseInt( input );
+            return true;
+        }
+        catch( Exception e ) {
+            return false;
+        }
+    }
     @GetMapping(path="/user/index")
 public  String patients(Model model, @RequestParam(name="page" , defaultValue = "0") int page
             , @RequestParam(name="size" , defaultValue = "5")int size  , @RequestParam(name="keyword" ,
-            defaultValue = "")  String keyword)
+            defaultValue = "")  String keyword )
 {
-    Page<Patient> patients = patientRepository.findByNomContains(keyword , PageRequest.of(page , size)) ;
+    int score;
+    Page<Patient> patients ;
+if(!keyword.equals("") && isInteger(keyword) ) {
+
+    score=Integer.valueOf(keyword) ;
+
+    System.out.println(" score :" + score + " keyword : "+keyword);
+     patients = patientRepository.findByNomOrScore(keyword , score , PageRequest.of(page , size)) ;
+}else {
+     patients = patientRepository.findByNomContains(keyword  , PageRequest.of(page , size)) ;
+}
+
     model.addAttribute("ListPatients",patients.getContent())  ;
     model.addAttribute("pages" , new int[patients.getTotalPages()]) ;
     model.addAttribute("currentPage" , page) ;
